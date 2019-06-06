00c901358c1b4d893141cd42319cbd9e1769e061
akalya23 <akalyaammu26@gmail.com>
2019-06-06 16:04:11 +0530

    Update 10c.py

--- 10c.py
+++ 10c.py
@@ -1,6 +1,7 @@
-N1 = int(input())
-Count = 0
-while(N1 > 0):
-    N1 = N1// 10
-    Count = Count + 1
-print(Count)
+   
+n1= int(input())
+c = 0
+while(n1 > 0):
+    n1= n1 // 10
+    c = c + 1
+print(c)
