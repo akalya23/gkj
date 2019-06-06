82d8e02c2dba9aeae1754ab603bb67857c407634
akalya23 <akalyaammu26@gmail.com>
2019-06-06 13:54:24 +0530

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,11 @@
+nm = int(input(""))  
+sum = 0  
+temp = nm  
+while temp > 0:  
+   digit = temp % 10  
+   sum += digit ** 3  
+   temp //= 10  
+if nm == sum:  
+   print("yes")  
+else:  
+   print("no")
